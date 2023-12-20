9f85b43d6f61f86be9bd32c848d994ed0fb85a29
Daniel-LA0303 <03daniel030399@gmail.com>
2023-12-20 17:47:36 -0600

    new time line and new navbar

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -12,100 +12,67 @@ const Experience = () => {
     <div className='text-white' id='experience'>
       <div className='px-5'>
         <p className='text-green-500 text-5xl mb-4'>Experience</p>
-
       </div>
-    
-      <div className="wrapper">
-        <div className="center-line">
-          {/* <a href="#" className="scroll-icon"><i className="fas fa-caret-up"></i></a> */}
+      <section className="timeline-section">
+        <div className="timeline-items">
+          <div className="timeline-item">
+            <div className="timeline-dot"></div>
+            <div className="timeline-date">2015</div>
+            <div className="timeline-content">
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
+          <div className="timeline-item">
+            <div className="timeline-dot"></div>
+            <div className="timeline-date">2016</div>
+            <div className="timeline-content">
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
+          <div className="timeline-item">
+            <div className="timeline-dot"></div>
+            <div className="timeline-date">2017</div>
+            <div className="timeline-content">
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
+          <div className="timeline-item">
+            <div className="timeline-dot"></div>
+            <div className="timeline-date">2018</div>
+            <div className="timeline-content">
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
+          <div className="timeline-item">
+            <div className="timeline-dot"></div>
+            <div className="timeline-date">2019</div>
+            <div className="timeline-content">
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
+          <div className="timeline-item">
+            <div className="timeline-dot"></div>
+            <div className="timeline-date">2020</div>
+            <div className="timeline-content">
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
+          <div className="timeline-item">
+            <div className="timeline-dot"></div>
+            <div className="timeline-date">2021</div>
+            <div className="timeline-content">
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
         </div>
-        <div className="row row-1">
-          <section>
-            <i className="icon fas fa-home"></i>
-            <div className="details">
-              <span className="title">Title of Section 1</span>
-              <span className=' text-sm sm:text-base'>1st Jan 2021</span>
-            </div>
-            <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores, sint eos vero sapiente voluptas debitis dicta dolore.</p>
-            <p>lo</p>
-            <div className="bottom">
-              <a href="#">Read more</a>
-              <i>- Someone famous</i>
-            </div>
-          </section>
-        </div>
-        <div className="row row-2">
-          <section>
-            <i className="icon fas fa-star"></i>
-            <div className="details">
-              <span className="title">Title of Section 2</span>
-              <span className=' text-sm sm:text-base'>2nd Jan 2021</span>
-            </div>
-            <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores, sint eos vero sapiente voluptas debitis dicta dolore.</p>
-            <div className="bottom">
-              <a href="#">Read more</a>
-              <i>- Someone famous</i>
-            </div>
-          </section>
-        </div>
-        <div className="row row-1">
-          <section>
-            <i className="icon fas fa-rocket"></i>
-            <div className="details">
-              <span className="title">Title of Section 3</span>
-              <span className=' text-sm sm:text-base'>3rd Jan 2021</span>
-            </div>
-            <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores, sint eos vero sapiente voluptas debitis dicta dolore.</p>
-            <div className="bottom">
-              <a href="#">Read more</a>
-              <i>- Someone famous</i>
-            </div>
-          </section>
-        </div>
-        <div className="row row-2">
-          <section>
-            <i className="icon fas fa-globe"></i>
-            <div className="details">
-              <span className="title">Title of Section 4</span>
-              <span className=' text-sm sm:text-base'>4th Jan 2021</span>
-            </div>
-            <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores, sint eos vero sapiente voluptas debitis dicta dolore.</p>
-            <div className="bottom">
-              <a href="#">Read more</a>
-              <i>- Someone famous</i>
-            </div>
-          </section>
-        </div>
-        <div className="row row-1">
-          <section>
-            <i className="icon fas fa-paper-plane"></i>
-            <div className="details">
-              <span className="title">Title of Section 5</span>
-              <span className=' text-sm sm:text-base'>5th Jan 2021</span>
-            </div>
-            <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores, sint eos vero sapiente voluptas debitis dicta dolore.</p>
-            <div className="bottom">
-              <a href="#">Read more</a>
-              <i>- Someone famous</i>
-            </div>
-          </section>
-        </div>
-        <div className="row row-2">
-          <section>
-            <i className="icon fas fa-map-marker-alt"></i>
-            <div className="details">
-              <span className="title">Title of Section 6</span>
-              <span className=' text-sm sm:text-base'>6th Jan 2021</span>
-            </div>
-            <p>Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed qui veroes praesentium maiores, sint eos vero sapiente voluptas debitis dicta dolore.</p>
-            <div className="bottom">
-              <a href="#">Read more</a>
-              <i>- Someone famous</i>
-            </div>
-          </section>
-        </div>
-      </div>
-
+      </section>
     </div>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,72 +1,71 @@
+import { faBars, faClose } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [menuAbierto, setMenuAbierto] = useState(false);
 
-  const toggleMenuOpen = () => {
-    setIsMenuOpen(!isMenuOpen);
-
-    // Agregar lógica para cambiar la clase 'open' en el elemento 'body'
-    if (isMenuOpen) {
-      document.body.classList.remove('open');
-    } else {
-      document.body.classList.add('open');
-    }
+  const toggleMenu = () => {
+    setMenuAbierto(!menuAbierto);
   };
 
   return (
     <>
-      <div className="navbar">
-        {isMenuOpen && (
-          <div className="navbar-overlay" onClick={toggleMenuOpen}></div>
-        )}
+      {/* New navbar */}
+      <header className='bg-black-card'>
 
-        <button type="button" className={`navbar-burger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenuOpen}>
-            <span className="material-icons">menu</span>
-        </button>
+        <div className=' text-green-500 text-base sm:text-2xl'>Daniel-LA | Developer</div>
 
+        <ul className={`navbar ${menuAbierto ? 'open' : ''}`}>
+          <li>
+            <Link
+              className=' cursor-pointer'
+              to='about-me'
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={500}
+            >About me</Link>
+          </li>
+          <li>
+            <Link
+              className=' cursor-pointer'
+              to='experience'
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={500}
+            >Experience</Link>
+          </li>
+          <li>
+            <Link
+              className=' cursor-pointer'
+              to='projects'
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={500}
+            >Projects</Link>
+          </li>
+          <li>
+            <Link
+              className=' cursor-pointer'
+              to='contact'
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={500}
+            >Contact</Link>
+          </li>
+        </ul>
 
-        <h1 className="navbar-title text-green-500">Daniel-LA | Developer</h1>
-        <nav className={`navbar-menu ${isMenuOpen ? 'open' : ''}`}>
-          <Link 
-            className=' cursor-pointer'
-            to='about-me'
-            spy={true}
-            smooth={true}
-            offset={-80}
-            duration={500}
-            onClick={toggleMenuOpen}
-          >About me</Link>
-          <Link
-            className=' cursor-pointer'
-            to='experience'
-            spy={true}
-            smooth={true}
-            offset={-80}
-            duration={500}           
-            onClick={toggleMenuOpen} 
-          >Experience</Link>
-          <Link
-            className=' cursor-pointer'
-            to='projects'
-            spy={true}
-            smooth={true}
-            offset={-80}
-            duration={500}    
-            onClick={toggleMenuOpen}      
-          >Projects</Link>
-          <Link
-            className=' cursor-pointer'
-            to='contact'
-            spy={true}
-            smooth={true}
-            offset={-80}
-            duration={500}   
-            onClick={toggleMenuOpen}       
-          >Contact</Link>
-        </nav>
-      </div>
+        <div class="main">
+          <div className={'menu-icon'} id="" onClick={toggleMenu}>
+            <FontAwesomeIcon icon={menuAbierto ? faClose : faBars} />
+          </div>
+        </div>
+      </header>
     </>
   );
 };
